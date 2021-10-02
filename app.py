489dbeba67e4205e496a38e2caff972245911274
ChrisGerritsen-HRO <1000806@hr.nl>
2021-10-02 23:33:28 +0200

    Part of image handeling

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request,url_for
+import flask
 from flask.templating import render_template
 from werkzeug.utils import redirect
 import sqlite3
+import os
 
 app = Flask(__name__)
 
 DATABASE = 'doodle.db'
+UPLOADFOLDER = 'images'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOADFOLDER
+
+def allow_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def main():

f187ab440f3266f211c9c01977c66ed079ca1d53
ChrisGerritsen-HRO <1000806@hr.nl>
2021-10-03 13:05:44 +0200

    register user

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, request,url_for
 import flask
+from flask.helpers import flash
 from flask.templating import render_template
-from werkzeug.utils import redirect
+from werkzeug.utils import redirect, secure_filename
 import sqlite3
 import os
 
 app = Flask(__name__)
 
+SECRET_KEY = os.urandom(24)
 DATABASE = 'doodle.db'
 UPLOADFOLDER = 'images'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-app = flask(__name__)
+# app = flask(__name__)
+app.secret_key = SECRET_KEY
 app.config['UPLOAD_FOLDER'] = UPLOADFOLDER
 
 def allow_file(filename):
@@ -21,9 +24,21 @@ def allow_file(filename):
 def main():
     return "<p>Index page!</p>"
 
-@app.route("/register", methods=["GET", "POST"])
+@app.route("/register", methods=["POST", "GET"])
 def register():
     if request.method == "POST":
+        # if 'file' not in request.files:
+        #     flash('No file part')
+        #     return redirect("/")
+        # file = request.file['file']
+
+        # if file.filename == '':
+        #     flash('No selected file')
+        #     return redirect("/")
+        # if file and allow_file(file.filename):
+        #     filename = secure_filename(file.filename)
+        #     file.save(os.path.join(app.config[UPLOADFOLDER], filename))
+
         try:
             nm = request.form['name']
             email = request.form['email']
@@ -34,13 +49,13 @@ def register():
 
             with sqlite3.connect(DATABASE) as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO user (name, email, birthday, profilepic, password, confirmpass) VALUES (?,?,?,?,?,?)", (nm, email, birthday, profilePic, passwrd, confirmPasswrd))
+                cur.execute("INSERT INTO user (name, email, birthday, profilepic, password) VALUES (?,?,?,?,?)", (nm, email, birthday, profilePic, passwrd))
                 
                 con.commit()
         except:
             pass
         finally:
             con.close()
-            return redirect(url_for(main))
+            return redirect("/")
 
     return render_template("register.html")
